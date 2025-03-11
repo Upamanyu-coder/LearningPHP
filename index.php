86ad9439462ab5ab2615635969a18f609d238337
Yubraj Dhakal <112164199+Upamanyu-coder@users.noreply.github.com>
2025-03-11 01:59:26 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Code</title>
+</head>
+<body>
+    <div>
+    This is my first PHP code
+    <?php
+    echo "Hello World";
+    ?>
+    </div>
+</body>
+</html>
